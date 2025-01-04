d4a07023830ddd5b5e9a9d8fcc476b89104e3c2f
Mohamed Amine Lotfi <medamineltf.100@gmail.com>
2025-01-04 12:38:27 +0100

    About section - cv

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -26,23 +26,107 @@ const AboutSection = () => {
                         typingSpeed={100}
                         deletingSpeed={50}
                         pauseTime={1500}
-                        className= 'text-gray-900'
+                        className='text-gray-900'
                     />
 
-                    <p className="text-white dark:text-teal-300 leading-relaxed mb-6 font-semibold">
-                        I am a Full-Stack Developer with expertise in creating dynamic and responsive web applications. As a Front-End Developer, I specialize in React.js and Next.js, building intuitive and user-friendly interfaces. On the Back-End, I leverage Java Spring Boot for developing robust server-side solutions. With a strong foundation in the MERN Stack (MongoDB, Express.js, React.js, and Node.js), I excel at developing scalable and efficient solutions tailored to meet client needs, always with a focus on clean code and user-centric design.
-                    </p>
+<p className="text-white dark:text-teal-300 leading-relaxed mb-6 font-semibold">
+    I am an experienced Full-Stack Developer with a proven track record of creating dynamic
+    and responsive web applications.
+    <br />
+    <br />
+    On the Front-End, I specialize in
+    <a
+        href="https://reactjs.org/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="dark:text-teal-400 text-gray-900 underline hover:dark:text-teal-600 hover:text-teal-600 transition duration-300"
+        title="Visit React.js"
+    >
+        React.js
+    </a>
+    and
+    <a
+        href="https://nextjs.org/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="dark:text-teal-400 text-gray-900 underline hover:dark:text-teal-600 hover:text-teal-600 transition duration-300"
+        title="Visit Next.js"
+    >
+        Next.js
+    </a>
+    , building intuitive, user-friendly interfaces.
+    <br />
+    <br />
+    On the Back-End, I bring expertise in
+    <a
+        href="https://spring.io/projects/spring-boot"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="dark:text-teal-400 text-gray-900 underline hover:dark:text-teal-600 hover:text-teal-600 transition duration-300"
+        title="Visit Java Spring Boot"
+    >
+        Java Spring Boot
+    </a>
+    , delivering robust, scalable, and secure server-side solutions. Additionally, I have extensive
+    experience with the
+    <a
+        href="https://www.mongodb.com/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="dark:text-teal-400 text-gray-900 underline hover:dark:text-teal-600 hover:text-teal-600 transition duration-300"
+        title="Visit MongoDB"
+    >
+        MongoDB
+    </a>
+    ,
+    <a
+        href="https://expressjs.com/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="dark:text-teal-400 text-gray-900 underline hover:dark:text-teal-600 hover:text-teal-600 transition duration-300"
+        title="Visit Express.js"
+    >
+        Express.js
+    </a>
+    ,
+    <a
+        href="https://reactjs.org/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="dark:text-teal-400 text-gray-900 underline hover:dark:text-teal-600 hover:text-teal-600 transition duration-300"
+        title="Visit React.js"
+    >
+        React.js
+    </a>
+    , and
+    <a
+        href="https://nodejs.org/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="dark:text-teal-400 text-gray-900 underline hover:dark:text-teal-600 hover:text-teal-600 transition duration-300"
+        title="Visit Node.js"
+    >
+        Node.js
+    </a>
+    , enabling me to develop comprehensive and efficient full-stack solutions.
+    <br />
+    <br />
+    I am committed to writing clean, maintainable code and designing user-centric applications
+    that provide exceptional performance and usability.
+</p>
+
+
 
                     <div className="flex flex-col sm:flex-row gap-4 justify-center">
                         <a
-                            href="/images/resume-english.pdf"
+                            href="/cv/cven10'24.pdf"
                             download="resume-english.pdf"
                             className="px-6 py-3 bg-gray-900 text-white text-lg font-medium rounded-lg hover:bg-gray-700 dark:bg-teal-500 dark:text-gray-900 dark:hover:bg-teal-400 transition duration-300"
                         >
                             Download English Resume
                         </a>
                         <a
-                            href="/images/resume-french.pdf"
+                            href="/cv/cvfr1124.pdf"
                             download="resume-french.pdf"
                             className="px-6 py-3 bg-gray-900 text-white text-lg font-medium rounded-lg hover:bg-gray-700 dark:bg-teal-500 dark:text-gray-900 dark:hover:bg-teal-400 transition duration-300"
                         >
--- src/components/ServiceSection.tsx
+++ src/components/ServiceSection.tsx
@@ -7,7 +7,7 @@ const ServiceSection = () => {
 
     return (
         <section
-            className={`services py-16 ${isDarkMode ? 'bg-gray-900 text-white' : 'bg-white text-gray-800'}`} 
+            className={`services py-16 ${isDarkMode ? 'bg-gray-900 text-white' : 'bg-white text-gray-800'}`}
             id="services"
         >
             <div className="container mx-auto text-center mb-12">
@@ -16,53 +16,68 @@ const ServiceSection = () => {
                 </h2>
             </div>
             <div className="services-container grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 px-4">
-                {/* Service Box 1 */}
-                <div className={`service-box p-6 rounded-lg shadow-lg hover:shadow-xl transition duration-300 ${isDarkMode ? 'bg-gray-700' : 'bg-gray-200'}`}>
-                    <div className="service-info">
-                        <h4 className={`text-2xl font-semibold ${isDarkMode ? 'text-teal-400' : 'text-teal-600'} mb-4`}>
-                            Full-Stack Web Development
-                        </h4>
-                        <p className={`${isDarkMode ? 'text-gray-400' : 'text-gray-800'}`}>
-                            I specialize in end-to-end development of modern web applications using the MERN stack, crafting custom solutions tailored to clients needs, whether it&apos;s e-commerce platforms, content management systems (CMS), or social networking sites. Leveraging React.js for dynamic user interfaces, Node.js and Express.js for server logic, and MongoDB for flexible data storage, I ensure seamless functionality and scalability across all projects.
-                        </p>
+                {/* Service Box */}
+                {[
+                    {
+                        title: 'Dynamic Web Applications',
+                        description:
+                            'Expertise in building responsive web applications using modern technologies, focusing on scalability and performance.',
+                        icon: '/icons/dynamic.png',
+                    },
+                    {
+                        title: 'Front-End Development',
+                        description:
+                            'Creating user-friendly interfaces with React.js and Next.js for intuitive and seamless user experiences.',
+                        icon: '/icons/front.png',
+                    },
+                    {
+                        title: 'Back-End Solutions',
+                        description:
+                            'Developing robust and secure server-side solutions with Java Spring Boot to handle complex business logic.',
+                        icon: '/icons/back.png',
+                    },
+                    {
+                        title: 'MERN Stack Expertise',
+                        description:
+                            'Proficiency in MongoDB, Express.js, React.js, and Node.js for developing efficient full-stack applications.',
+                        icon: '/icons/MERN-logo.png',
+                    },
+                    {
+                        title: 'Clean Code Practices',
+                        description:
+                            'Commitment to writing maintainable code and ensuring best practices for long-term project success.',
+                        icon: '/icons/clean.png',
+                    },
+                    {
+                        title: 'User-Centric Design',
+                        description:
+                            'Designing applications that prioritize performance and usability to meet end-user needs effectively.',
+                        icon: '/icons/user.png',
+                    },
+                ].map((service, index) => (
+                    <div
+                        key={index}
+                        className={`service-box p-6 rounded-lg shadow-lg hover:shadow-xl transition duration-300 ${isDarkMode ? 'bg-gray-700' : 'bg-gray-200'
+                            }`}
+                    >
+                        <div className="service-info text-center">
+                            <img
+                                src={service.icon}
+                                alt={`${service.title} icon`}
+                                className="w-30 h-16 mx-auto mb-4"
+                            />
+                            <h4
+                                className={`text-2xl font-semibold mb-4 hover:underline ${isDarkMode ? 'text-teal-400' : 'text-teal-600'
+                                    }`}
+                            >
+                                {service.title}
+                            </h4>
+                            <p className={`${isDarkMode ? 'text-gray-400' : 'text-gray-800'}`}>
+                                {service.description}
+                            </p>
+                        </div>
                     </div>
-                </div>
-
-                {/* Service Box 2 */}
-                <div className={`service-box p-6 rounded-lg shadow-lg hover:shadow-xl transition duration-300 ${isDarkMode ? 'bg-gray-700' : 'bg-gray-200'}`}>
-                    <div className="service-info">
-                        <h4 className={`text-2xl font-semibold ${isDarkMode ? 'text-teal-400' : 'text-teal-600'} mb-4`}>
-                            Mobile App Development
-                        </h4>
-                        <p className={`${isDarkMode ? 'text-gray-400' : 'text-gray-800'}`}>
-                            Specializing in mobile app development using React Native, I craft custom solutions tailored to clients needs, ensuring native-like performance and user experience across iOS and Android platforms. Leveraging the power of React Native, I develop feature-rich applications that seamlessly integrate with device capabilities.
-                        </p>
-                    </div>
-                </div>
-
-                {/* Service Box 3 */}
-                <div className={`service-box p-6 rounded-lg shadow-lg hover:shadow-xl transition duration-300 ${isDarkMode ? 'bg-gray-700' : 'bg-gray-200'}`}>
-                    <div className="service-info">
-                        <h4 className={`text-2xl font-semibold ${isDarkMode ? 'text-teal-400' : 'text-teal-600'} mb-4`}>
-                            API Development and Integration
-                        </h4>
-                        <p className={`${isDarkMode ? 'text-gray-400' : 'text-gray-800'}`}>
-                            With expertise in Node.js and Express.js, I create robust RESTful APIs to power backend services for web and mobile applications, seamlessly integrating third-party APIs to enhance functionality. By implementing secure communication and data validation mechanisms, I ensure seamless data exchange and integration.
-                        </p>
-                    </div>
-                </div>
-
-                {/* Service Box 4 */}
-                <div className={`service-box p-6 rounded-lg shadow-lg hover:shadow-xl transition duration-300 ${isDarkMode ? 'bg-gray-700' : 'bg-gray-200'}`}>
-                    <div className="service-info">
-                        <h4 className={`text-2xl font-semibold ${isDarkMode ? 'text-teal-400' : 'text-teal-600'} mb-4`}>
-                            Integration and Deployment (CI/CD)
-                        </h4>
-                        <p className={`${isDarkMode ? 'text-gray-400' : 'text-gray-800'}`}>
-                            I streamline the development, testing, and deployment processes for MERN stack applications by setting up automated CI/CD pipelines. By configuring build automation tools and deployment strategies, I minimize downtime and risk during deployments, monitoring pipeline metrics to optimize workflow efficiency and enhance development agility.
-                        </p>
-                    </div>
-                </div>
+                ))}
             </div>
         </section>
     );
